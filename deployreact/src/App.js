1c207514ec1e57d480c0322ae35ee13c0077d94f
LucasNSecco <lucasnsecco@hotmail.com>
2025-03-01 09:08:07 -0300

    teste2

--- deployreact/src/App.js
+++ deployreact/src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p> testeeeee</p>
+       <p> testeeeee2</p>
         <a
           className="App-link"
           href="https://reactjs.org"
